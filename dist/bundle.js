447be4c47f79bb1ba2060996d6bbcb34bd7e08ef
RS885 <rebecca.scharl@iubh.de>
2024-10-07 16:04:17 +0200

    setup firebase v2

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const firebaseConfig = {\r\n    apiKey: \"AIzaSyARCmMDIU5LPjBMj_v_tYrhcUh8Aloynpo\",\r\n    authDomain: \"isef01-quizius-maximus-v2.firebaseapp.com\",\r\n    projectId: \"isef01-quizius-maximus-v2\",\r\n    storageBucket: \"isef01-quizius-maximus-v2.appspot.com\",\r\n    messagingSenderId: \"555791080708\",\r\n    appId: \"1:555791080708:web:c890c0d237eb00c2764451\"\r\n  };\r\n\r\n  \r\n\n\n//# sourceURL=webpack://isef01/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARCmMDIU5LPjBMj_v_tYrhcUh8Aloynpo",
+    authDomain: "isef01-quizius-maximus-v2.firebaseapp.com",
+    projectId: "isef01-quizius-maximus-v2",
+    storageBucket: "isef01-quizius-maximus-v2.appspot.com",
+    messagingSenderId: "555791080708",
+    appId: "1:555791080708:web:c890c0d237eb00c2764451"
+  };
+
+  
