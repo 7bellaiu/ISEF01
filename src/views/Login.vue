2c8735bba15221d836a3e5f28e76f22eab01f63e
7bellaiu <daniela.bodner@iu-study.org>
2024-11-02 19:17:47 +0100

    Toast-Komponente und Login-View inklusive Einbinden von Firebase-Auth

---
+++ src/views/Login.vue
@@ -0,0 +1,91 @@
+<script setup>
+/** IMPORTS */
+import { ref, onMounted } from "vue";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { useRouter } from "vue-router";
+import Toast from "@/components/Toast.vue";
+
+/** COMPONENT REFERENCES */
+const router = useRouter();
+const toastRef = ref(null);
+
+/** STATES */
+const email = ref("");
+const password = ref("");
+const toastMessage = ref("");
+const toastVariant = ref("");
+const userLoggedIn = ref(false);
+const userName = ref("");
+
+/** */
+onMounted(() => {
+    onAuthStateChanged(getAuth(), function (user) {
+        if (!user) {
+            userLoggedIn.value = false;
+        } else {
+            userLoggedIn.value = true;
+            userName.value = user.email;
+        }
+    });
+});
+
+/** HANDLER */
+const logIn = () => {
+    signInWithEmailAndPassword(getAuth(), email.value, password.value)
+        .then((data) => {
+            //console.log("Successfully logged in!", data);
+            toastMessage.value = "Login was successfull!";
+            toastVariant.value = "success";
+            triggerToast();
+        })
+        .catch((error) => {
+            //TBD: Security --> Fehlermeldung sollte keine Info an Angreifer geben
+            toastMessage.value = "Login failed!";
+            toastVariant.value = "danger";
+            triggerToast();
+        });
+};
+
+/** UTILITY */
+const triggerToast = () => {
+    if (toastRef.value) {
+        toastRef.value.showToast(); // Methode aufrufen, um das Toast zu aktivieren
+    }
+};
+</script>
+
+<template>
+    <main class="d-flex justify-content-center">
+        <div v-if="!userLoggedIn" class="p-3 mt-3 mb-3" style="width: 100%; max-width: 400px">
+            <!-- Logo -->
+            <img src="../assets/logo.jpeg" alt="Logo" class="mb-3" width="55%" height="25%" />
+            <!-- Titel -->
+            <h2 class="mb-3">Please sign in</h2>
+
+            <!-- Login-Formular -->
+            <form @submit.prevent="logIn" class="bg-body-tertiary">
+                <div class="mb-2">
+                    <input type="email" class="form-control" id="email" placeholder="Email address"
+                        data-ddg-inputtype="credentials.username" v-model="email" required />
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control" id="password" placeholder="Password"
+                        data-ddg-inputtype="credentials.password.current" v-model="password" required />
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button class="btn btn-primary w-100">Login</button>
+                </div>
+            </form>
+
+            <!-- Toast Success/Failure -->
+            <Toast ref="toastRef" :message="toastMessage" :variant="toastVariant" />
+        </div>
+
+        <fieldset v-else>
+            <legend>Welcome {{ userName }}!</legend>
+            <div class="d-flex justify-content-center">
+                <button @click="signOut(getAuth());" class="btn btn-danger w-100">Logout</button>
+            </div>
+        </fieldset>
+    </main>
+</template>
