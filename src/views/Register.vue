18e636070f3a5986829412672de37f556b09a4fc
7bellaiu <daniela.bodner@iu-study.org>
2024-11-02 19:26:47 +0100

    Register View inklusive Firebase Einbindung

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,8 +1,8 @@
 <script setup>
 /** IMPORTS */
 import { ref, onMounted } from "vue";
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { useRouter } from "vue-router";
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signOut } from "firebase/auth";
 import Toast from "@/components/Toast.vue";
 
 /** COMPONENT REFERENCES */
@@ -14,6 +14,7 @@ const email = ref("");
 const password = ref("");
 const toastMessage = ref("");
 const toastVariant = ref("");
+const showToast = ref(false);
 const userLoggedIn = ref(false);
 const userName = ref("");
 
@@ -25,21 +26,28 @@ onMounted(() => {
         } else {
             userLoggedIn.value = true;
             userName.value = user.email;
+            router.push("/login");
         }
     });
 });
 
-/** HANDLER */
-const logIn = () => {
-    signInWithEmailAndPassword(getAuth(), email.value, password.value)
+const register = () => {
+    createUserWithEmailAndPassword(getAuth(), email.value, password.value)
         .then((data) => {
-            //console.log("Successfully logged in!", data);
-            toastMessage.value = "Login was successfull!";
+            toastMessage.value = "Registration was successfull!";
             toastVariant.value = "success";
             triggerToast();
         })
         .catch((error) => {
-            toastMessage.value = "Login failed!";   //give no information bc hackers
+            switch (error.code) {
+                case "auth/weak-password":
+                    toastMessage.value = "Your Password should be at least 6 characters long";
+                    break;
+
+                default:
+                    toastMessage.value = error.code;
+                    break;
+            }
             toastVariant.value = "danger";
             triggerToast();
         });
@@ -59,10 +67,10 @@ const triggerToast = () => {
             <!-- Logo -->
             <img src="../assets/logo.jpeg" alt="Logo" class="mb-3" width="55%" height="25%" />
             <!-- Titel -->
-            <h2 class="mb-3">Please sign in</h2>
+            <h2 class="mb-3">Please register</h2>
 
             <!-- Login-Formular -->
-            <form @submit.prevent="logIn" class="bg-body-tertiary">
+            <form @submit.prevent="register" class="bg-body-tertiary">
                 <div class="mb-2">
                     <input type="email" class="form-control" id="email" placeholder="Email address"
                         data-ddg-inputtype="credentials.username" v-model="email" required />
@@ -72,14 +80,13 @@ const triggerToast = () => {
                         data-ddg-inputtype="credentials.password.current" v-model="password" required />
                 </div>
                 <div class="d-flex justify-content-center">
-                    <button class="btn btn-primary w-100">Login</button>
+                    <button class="btn btn-primary w-100">Register</button>
                 </div>
             </form>
 
-            <!-- Toast Success/Failure -->
+            <!-- Erfolgs-/Fehlermeldung -->
             <Toast ref="toastRef" :message="toastMessage" :variant="toastVariant" />
         </div>
-
         <fieldset v-else>
             <legend>Welcome {{ userName }}!</legend>
             <div class="d-flex justify-content-center">
